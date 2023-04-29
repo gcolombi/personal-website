40c0fcc40efb189a488b05a536a91ef25ae51599
gcolombi <gg_1605@hotmail.fr>
2023-04-29 19:14:44 -0400

    update _document - add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+                <link rel="manifest" href="/static/favicons/site.webmanifest" />
+                <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#000000" />
+                <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
