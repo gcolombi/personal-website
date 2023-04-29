31483ea2093b68a17de3e47742dc07d669723347
gcolombi <gg_1605@hotmail.fr>
2023-04-29 18:49:43 -0400

    update _app - removes focus after page change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    /* Removes focus from next/link element after page change */
+    useEffect(() => {
+        document.activeElement instanceof HTMLElement && document.activeElement.blur();
+    }, [router]);
+
     return <Component {...pageProps} />
 }
