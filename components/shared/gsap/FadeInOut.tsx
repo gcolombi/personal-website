9bb0d4a01a2d0446f0c3fbb4480b73f5036ed2c3
gcolombi <gg_1605@hotmail.fr>
2023-04-30 19:15:07 -0400

    add FadeInOut component

---
+++ components/shared/gsap/FadeInOut.tsx
@@ -0,0 +1,41 @@
+import { Fade } from '@/types/animations';
+import AnimateInOut from './AnimateInOut';
+
+export default function FadeInOut({
+    children,
+    durationIn = 0.5,
+    durationOut = 0.25,
+    delay = 0,
+    delayOut = 0,
+    ease = 'power4.out',
+    skipOutro,
+    watch,
+    start = 'top bottom',
+    end = 'bottom top',
+    scrub = false,
+    markers
+}: Fade) {
+    return (
+        <AnimateInOut
+            durationIn={durationIn}
+            durationOut={durationOut}
+            delay={delay}
+            delayOut={delayOut}
+            from={{
+                opacity: 0
+            }}
+            to={{
+                ease,
+                opacity: 1
+            }}
+            skipOutro={skipOutro}
+            watch={watch}
+            start={start}
+            end={end}
+            scrub={scrub}
+            markers={markers}
+        >
+            {children}
+        </AnimateInOut>
+    );
+}
