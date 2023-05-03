f610b4a052a2194de9520d688a9509eb7a5a8493
gcolombi <gg_1605@hotmail.fr>
2023-05-03 19:55:06 -0400

    add Form component - WIP & remove dead scss modules

---
+++ components/form/Form.tsx
@@ -0,0 +1,9 @@
+export default function Form() {
+    return (
+        <section>
+            <div className="o-container">
+                <h1>Form</h1>
+            </div>
+        </section>
+    );
+};
--- pages/form.tsx
+++ pages/form.tsx
@@ -1,6 +1,18 @@
-export default function Form() {
+import MetaData from '@/components/MetaData';
+import HeaderBasic from '@/components/HeaderBasic';
+import Form from '@/components/form/Form';
+
+export default function FormPage() {
     return (
         <>
+            <MetaData
+                title="Form"
+            />
+            <HeaderBasic
+                title="Form"
+                wysiwyg="Complete and flexible form with Google ReCaptcha V3, ready to use. Form fields are handled by React Hook Form and validated by Yup on the client/server side. The form request is managed by an API route, SendGrid and a custom HTML template are used to send the email."
+            />
+            <Form />
         </>
     );
 };
