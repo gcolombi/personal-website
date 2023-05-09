3b30ba930fd785d73dcca52edac625eacea42603
gcolombi <gg_1605@hotmail.fr>
2023-05-08 23:47:00 -0400

    update _container

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
                 <div className="o-container">
                     <div className="o-wysiwyg">
                         <h1>Homepage</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi?</p>
                     </div>
                 </div>
             </section>
