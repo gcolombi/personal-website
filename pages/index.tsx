70a0055a96ab04a9ec047bd1528fa7ce686b7453
gcolombi <gg_1605@hotmail.fr>
2023-05-09 00:38:34 -0400

    update heading & text - WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,11 @@ export default function Home() {
             <section className="u-spacing--responsive">
                 <div className="o-container">
                     <div className="o-wysiwyg">
-                        <h1>Homepage</h1>
+                        <h1>
+                            Front-end
+                            <br/>
+                            Developer
+                        </h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, perspiciatis? Placeat dolore, eveniet ipsum nesciunt dolorem quisquam incidunt facere vero. Totam at ipsa iste ipsum sed maiores ab eos quasi?</p>
                     </div>
                 </div>
