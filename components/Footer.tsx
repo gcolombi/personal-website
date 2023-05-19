49c4c21d3b945f898c292bb84d4f73df2fd83dea
gcolombi <gg_1605@hotmail.fr>
2023-05-18 23:40:04 -0400

    update Footer - WIP

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,59 @@
 import styles from '@/styles/modules/Footer.module.scss';
+import classNames from 'classnames';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <footer className={styles['c-footer']}>
-            <div className="o-container u-text--center o-wysiwyg">
-                <p>&copy; {new Date().getFullYear()} Next.js. All rights reserved.</p>
+            <div className="o-container">
+                <div className={styles['c-footer__row']}>
+                    <div className={classNames
+                        (
+                            'o-wysiwyg',
+                            styles['c-footer__copyright']
+                        )}
+                    >
+                        <p>&copy; {new Date().getFullYear()}</p>
+                    </div>
+                    <div className={styles['c-footer__socialLinks']}>
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://www.instagram.com/"
+                                >
+                                    Instagram
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://twitter.com/"
+                                >
+                                    Twitter
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/"
+                                >
+                                    Github
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={classNames
+                        (
+                            'o-wysiwyg',
+                            styles['c-footer__title']
+                        )}
+                    >
+                        <Link href="/" title="Gerard Colombi">
+                            Gerard Colombi
+                        </Link>
+                        <p>Front-end developer</p>
+                    </div>
+                </div>
+
+
             </div>
         </footer>
     );
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -91,7 +91,7 @@ export default function Navigation() {
                 <div className="o-container">
                     <div className={styles['c-navigation__row']}>
                         <div className={styles['c-navigation__logo']}>
-                            <Link href="/" title="Gerard Colombi">
+                            <Link href="/" aria-label="Gerard Colombi">
                                 Gerard Colombi
                             </Link>
                         </div>
