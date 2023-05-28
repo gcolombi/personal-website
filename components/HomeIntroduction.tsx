805a1598c4e5c5ec636908e5c12a3f65342a36ec
gcolombi <gg_1605@hotmail.fr>
2023-05-28 17:23:13 -0400

    update HomeIntroduction - add animations

--- components/HomeIntroduction.tsx
+++ components/HomeIntroduction.tsx
@@ -1,6 +1,8 @@
 import styles from '@/styles/modules/HomeIntroduction.module.scss';
 import SectionHeader from './shared/SectionHeader';
 import Button from './shared/Button';
+import LinesInOut from './shared/gsap/LinesInOut';
+import FadeInOut from './shared/gsap/FadeInOut';
 import classNames from 'classnames';
 
 export default function HomeIntroduction() {
@@ -14,25 +16,43 @@ export default function HomeIntroduction() {
                 <div className="o-grid">
                     <div className={classNames(
                         'u-large-text',
-                        'o-wysiwyg',
                         styles['c-homeIntroduction__title']
                     )}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At cupiditate maxime accusamus deserunt tempore quam odio in sapiente obcaecati perspiciatis.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolorum facilis libero enim officia commo.</p>
+                        <LinesInOut
+                            target="#title-beginning"
+                            watch
+                        >
+                            <p id="title-beginning">Lorem ipsum dolor sit amet consectetur adipisicing elit. At cupiditate maxime accusamus deserunt tempore quam odio in sapiente obcaecati perspiciatis.</p>
+                        </LinesInOut>
+                        <LinesInOut
+                            target="#title-end"
+                            watch
+                        >
+                            <div className="o-wysiwyg">
+                                <p id="title-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolorum facilis libero enim officia commo.</p>
+                            </div>
+                        </LinesInOut>
                     </div>
-                    <div className={classNames(
-                        'o-wysiwyg',
-                        styles['c-homeIntroduction__content']
-                    )}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias cumque odit quod aut? Molestiae labore deleniti aperiam commodi quos, nam id, tenetur illo sit iste, vel iure. Nulla, eius. Molestiae labore deleniti aperiam modus.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quos doloribus maiores laboriosam aspernatur voluptatibus.</p>
+                    <div className={styles['c-homeIntroduction__content']}>
+                        <FadeInOut
+                            watch
+                        >
+                            <div className="o-wysiwyg">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias cumque odit quod aut? Molestiae labore deleniti aperiam commodi quos, nam id, tenetur illo sit iste, vel iure. Nulla, eius. Molestiae labore deleniti aperiam modus.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quos doloribus maiores laboriosam aspernatur voluptatibus.</p>
+                            </div>
+                        </FadeInOut>
                     </div>
                     <div className={styles['c-homeIntroduction__btn']}>
-                        <Button
-                            label="More about me"
-                            href="/about"
-                            className="c-btn"
-                        />
+                        <FadeInOut
+                            watch
+                        >
+                            <Button
+                                label="More about me"
+                                href="/about"
+                                className="c-btn"
+                            />
+                        </FadeInOut>
                     </div>
                 </div>
             </div>
