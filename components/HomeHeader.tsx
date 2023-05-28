0a881856192f3f0ffe752e338a011303576938b3
gcolombi <gg_1605@hotmail.fr>
2023-05-28 15:19:19 -0400

    add HomeHeader - WIP

---
+++ components/HomeHeader.tsx
@@ -0,0 +1,47 @@
+import styles from '@/styles/modules/HomeHeader.module.scss';
+import CharsInOut from "./shared/gsap/CharsInOut";
+
+export default function HomeHeader() {
+    return (
+        <section>
+            <div className="o-container">
+                <div className="o-grid">
+                    <h1>
+                        <CharsInOut
+                            delay={0.46}
+                            target="#chars-1"
+                        >
+                            <span id="chars-1">
+                                Gerard
+                            </span>
+                        </CharsInOut>
+                        <CharsInOut
+                            delay={0.46}
+                            target="#chars-2"
+                        >
+                            <span id="chars-2">
+                                Colombi
+                            </span>
+                        </CharsInOut>
+                        <CharsInOut
+                            delay={0.46}
+                            target="#chars-3"
+                        >
+                            <span id="chars-3">
+                                Front-end
+                            </span>
+                        </CharsInOut>
+                        <CharsInOut
+                            delay={0.46}
+                            target="#chars-4"
+                        >
+                            <span id="chars-4">
+                                Developer
+                            </span>
+                        </CharsInOut>
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
+};
