cdf58f38a583270f396650a910f36aec705c1768
gcolombi <gg_1605@hotmail.fr>
2023-05-30 09:35:35 -0400

    update HomeFeaturedWork - add stylesheet & head

--- components/HomeFeaturedWork.tsx
+++ components/HomeFeaturedWork.tsx
@@ -1,13 +1,46 @@
-import SectionHeader from './shared/SectionHeader';
+import styles from '@/styles/modules/HomeFeaturedWork.module.scss';
+import FadeInOut from './shared/gsap/FadeInOut';
+import TranslateInOut from './shared/gsap/TranslateInOut';
+import classNames from 'classnames';
 
 export default function HomeFeaturedWork() {
     return(
         <section className="u-spacing--responsive">
             <div className="o-container">
-                <SectionHeader
-                    index="02"
-                    title="Featured work"
-                />
+                <div className="o-grid">
+                <div className={classNames(
+                    'h4',
+                    styles['c-homeFeaturedWork__index']
+                )}>
+                    <FadeInOut
+                        watch
+                    >
+                        <span>
+                            02
+                        </span>
+                    </FadeInOut>
+                </div>
+                <div className={classNames(
+                    'u-overflow--hidden',
+                    'u-uppercase',
+                    styles['c-homeFeaturedWork__title']
+                )}>
+                    <TranslateInOut
+                        fade={false}
+                        y="100%"
+                        start="-100% bottom"
+                        end="top top"
+                        watch
+                    >
+                        <h2 className={classNames(
+                            'h4',
+                            'u-margin--none',
+                        )}>
+                            Featured work
+                        </h2>
+                    </TranslateInOut>
+                </div>
+                </div>
             </div>
         </section>
     );
