4dd26755b467903777b8f899108919e332d1f4ef
gcolombi <gg_1605@hotmail.fr>
2023-05-30 11:34:34 -0400

    update AboutHeader - style

--- components/AboutIntroduction.tsx
+++ components/AboutIntroduction.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/modules/AboutIntroduction.module.scss';
 import FadeInOut from './shared/gsap/FadeInOut';
+import LinesInOut from './shared/gsap/LinesInOut';
 import classNames from 'classnames';
 
 export default function AboutIntroduction() {
@@ -23,16 +24,26 @@ export default function AboutIntroduction() {
                         </FadeInOut>
                     </div>
                     <div className={styles['c-aboutIntroduction__content']}>
-                        <FadeInOut
+                        <LinesInOut
+                            target="#title-beginning"
+                            watch
+                        >
+                            <p id="title-beginning">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias cumque odit quod aut? Molestiae labore deleniti aperiam commodi quos, nam id, tenetur illo sit iste, vel iure. Nulla, eius. Molestiae labore deleniti aperiam modus.</p>
+                        </LinesInOut>
+                        <LinesInOut
+                            target="#title-end"
                             watch
                         >
                             <div className="o-wysiwyg">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias cumque odit quod aut? Molestiae labore deleniti aperiam commodi quos, nam id, tenetur illo sit iste, vel iure. Nulla, eius. Molestiae labore deleniti aperiam modus.</p>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quos doloribus maiores laboriosam aspernatur voluptatibus.</p>
+                                <p id="title-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quos doloribus maiores laboriosam aspernatur voluptatibus.</p>
                             </div>
-                        </FadeInOut>
+                        </LinesInOut>
                     </div>
-
+                    <div className={styles['c-aboutIntroduction__media']}>
+                        <picture>
+                            <img src="https://source.unsplash.com/422x677?tech" alt="" />
+                        </picture>
+                    </div>
 
                 </div>
             </div>
