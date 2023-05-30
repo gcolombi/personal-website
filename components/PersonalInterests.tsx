6e810ec79d210dacae08ec375ea06fcdd1f1418b
gcolombi <gg_1605@hotmail.fr>
2023-05-30 10:10:31 -0400

    update PersonalInterests - add head & update stylesheet

--- components/PersonalInterests.tsx
+++ components/PersonalInterests.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/modules/PersonalInterests.module.scss';
-import SectionHeader from './shared/SectionHeader';
+import FadeInOut from './shared/gsap/FadeInOut';
+import TranslateInOut from './shared/gsap/TranslateInOut';
 import classNames from 'classnames';
 
 export default function PersonalInterests() {
@@ -9,11 +10,39 @@ export default function PersonalInterests() {
             styles['c-personalInterests']
         )}>
             <div className="o-container">
-                <SectionHeader
-                    index="02"
-                    title="Personal Interests"
-                />
                 <div className="o-grid">
+                    <div className={classNames(
+                        'h4',
+                        styles['c-personalInterests__index']
+                    )}>
+                        <FadeInOut
+                            watch
+                        >
+                            <span>
+                                02
+                            </span>
+                        </FadeInOut>
+                    </div>
+                    <div className={classNames(
+                        'u-overflow--hidden',
+                        'u-uppercase',
+                        styles['c-personalInterests__title']
+                    )}>
+                        <TranslateInOut
+                            fade={false}
+                            y="100%"
+                            start="-100% bottom"
+                            end="top top"
+                            watch
+                        >
+                            <h2 className={classNames(
+                                'h4',
+                                'u-margin--none',
+                            )}>
+                                Personal Interests
+                            </h2>
+                        </TranslateInOut>
+                    </div>
 
                 </div>
             </div>
