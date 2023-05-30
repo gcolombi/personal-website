24be684e5c6ae2844a7ef721aa100b9690d69f0f
gcolombi <gg_1605@hotmail.fr>
2023-05-29 20:17:27 -0400

    update AboutHeader - WIP

--- components/AboutHeader.tsx
+++ components/AboutHeader.tsx
@@ -1,11 +1,39 @@
 import styles from '@/styles/modules/AboutHeader.module.scss';
+import CharsInOut from './shared/gsap/CharsInOut';
 
 export default function AboutHeader() {
     return (
         <section className={styles['c-aboutHeader']}>
             <div className="o-container">
                 <div className="o-grid">
-                    <h1>About header</h1>
+                    <div className={styles['c-aboutHeader__title']}>
+                        <h1 className="u-margin--none">
+                            <CharsInOut
+                                delay={0.46}
+                                target="#chars-1"
+                            >
+                                <span id="chars-1">
+                                    Passionate
+                                </span>
+                            </CharsInOut>
+                            <CharsInOut
+                                delay={0.46}
+                                target="#chars-2"
+                            >
+                                <span id="chars-2">
+                                    Front-end
+                                </span>
+                            </CharsInOut>
+                            <CharsInOut
+                                delay={0.46}
+                                target="#chars-3"
+                            >
+                                <span id="chars-3">
+                                    Developer
+                                </span>
+                            </CharsInOut>
+                        </h1>
+                    </div>
                 </div>
             </div>
         </section>
