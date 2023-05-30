922f657d4a860c9c285da8e65daa7909227e490a
gcolombi <gg_1605@hotmail.fr>
2023-05-30 10:05:35 -0400

    update AboutIntroduction - add stylesheet & head

--- components/AboutIntroduction.tsx
+++ components/AboutIntroduction.tsx
@@ -1,19 +1,29 @@
 import styles from '@/styles/modules/AboutIntroduction.module.scss';
-import SectionHeader from './shared/SectionHeader';
+import FadeInOut from './shared/gsap/FadeInOut';
 import classNames from 'classnames';
 
 export default function AboutIntroduction() {
     return (
         <section className={classNames(
-            'u-spacing--responsive',
+            'u-spacing--responsive--bottom',
             styles['c-aboutIntroduction']
         )}>
             <div className="o-container">
-                <SectionHeader
-                    index="01"
-                    title="Maker of things"
-                />
                 <div className="o-grid">
+                    <div className={classNames(
+                        'h4',
+                        styles['c-aboutIntroduction__index']
+                    )}>
+                        <FadeInOut
+                            watch
+                        >
+                            <span>
+                                01
+                            </span>
+                        </FadeInOut>
+                    </div>
+
+
 
                 </div>
             </div>
