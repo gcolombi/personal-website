996d9d139001ff108939d18795e2dd88254eafdd
gcolombi <gg_1605@hotmail.fr>
2023-05-30 20:29:11 -0400

    update HomeHeader - add ClipPath animation

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/modules/HomeHeader.module.scss';
 import CharsInOut from "./shared/gsap/CharsInOut";
+import ClipPathInOut from './shared/gsap/ClipPathInOut';
 import classNames from 'classnames';
 
 export default function HomeHeader() {
@@ -34,13 +35,21 @@ export default function HomeHeader() {
                         </h1>
                     </div>
                     <div className={styles['c-homeHeader__media']}>
-                        <div className={styles['c-homeHeader__media--img']}>
-                            <picture>
-                                <source srcSet="https://source.unsplash.com/350x350?tech" media="(max-width: 767px)" />
-                                <source srcSet="https://source.unsplash.com/250x350?tech" media="(max-width: 1449px)" />
-                                <img src="https://source.unsplash.com/380x500?tech" alt="" />
-                            </picture>
-                        </div>
+                        <ClipPathInOut
+                            fade={false}
+                            delay={0.46}
+                            clipPath="inset(100% 0% 0% 0%)"
+                            clipPathOut="inset(0% 0% 100% 0%)"
+                            watch
+                        >
+                            <div className={styles['c-homeHeader__media--img']}>
+                                <picture>
+                                    <source srcSet="https://source.unsplash.com/350x350?tech" media="(max-width: 767px)" />
+                                    <source srcSet="https://source.unsplash.com/250x350?tech" media="(max-width: 1449px)" />
+                                    <img src="https://source.unsplash.com/380x500?tech" alt="" />
+                                </picture>
+                            </div>
+                        </ClipPathInOut>
                     </div>
                     <div className={styles['c-homeHeader__name']}>
                         <h2 className="h1 u-margin--none">
