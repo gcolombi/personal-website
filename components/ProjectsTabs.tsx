4fc05c827c1019c32fcd795b14d27e0f0a6aea7c
gcolombi <gg_1605@hotmail.fr>
2023-06-02 16:45:01 -0400

    ProjectsTabs - handle description height

--- components/ProjectsTabs.tsx
+++ components/ProjectsTabs.tsx
@@ -1,6 +1,8 @@
 import styles from '@/styles/modules/ProjectsTabs.module.scss';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import { useRef, useState } from 'react';
-import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+import useTransitionContext from '@/context/transitionContext';
 import FadeInOut from './shared/gsap/FadeInOut';
 import CharsInOut from './shared/gsap/CharsInOut';
 import TranslateInOut from './shared/gsap/TranslateInOut';
@@ -8,24 +10,28 @@ import Project from './Project';
 import classNames from 'classnames';
 
 export default function ProjectsTabs() {
+    const { primaryEase } = useTransitionContext();
     const [activeTabIndex, setActiveTabIndex] = useState(0);
-    const tabsItemsRef = useRef<HTMLDivElement[] | null[]>([]);
-    const [tabsWrapperHeight, setTabsWrapperHeight] = useState<number | undefined>(0);
+    const tabsItemsRef = useRef<HTMLDivElement[]>([]);
+    const tabsWrapperRef = useRef<HTMLDivElement | null>(null);
 
     const handleClick = (index: number) => {
         setActiveTabIndex(index);
-        tabsItemsRef.current.forEach(item => {
-            if (parseInt(item?.dataset.id!) === index) {
-                console.log(item);
-                console.log(item?.scrollHeight);
-                setTabsWrapperHeight(item?.scrollHeight);
+        gsap.fromTo(tabsWrapperRef.current,
+            {
+                height: tabsWrapperRef.current?.offsetHeight
+            },
+            {
+            duration: 0.7,
+            // delay: 0.1,
+            // height: item?.scrollHeight,
+            height: 'auto',
+            ease: primaryEase,
+            onComplete: () => {
+                ScrollTrigger.refresh(true);
             }
         });
     }
-
-    useIsomorphicLayoutEffect(() => {
-        setTabsWrapperHeight(tabsItemsRef.current[activeTabIndex]?.scrollHeight);
-    }, []);
 
     return (
         <>
@@ -53,7 +59,6 @@ export default function ProjectsTabs() {
                                         [styles['is-active']]: activeTabIndex == 0
                                     }
                                 )}
-                                // onClick={() => setActiveTabIndex(0)}
                                 onClick={() => handleClick(0)}
                             >
                                 <div className="h1">
@@ -87,7 +92,6 @@ export default function ProjectsTabs() {
                                         [styles['is-active']]: activeTabIndex == 1
                                     }
                                 )}
-                                // onClick={() => setActiveTabIndex(1)}
                                 onClick={() => handleClick(1)}
                             >
                                 <div className="h1">
@@ -114,36 +118,40 @@ export default function ProjectsTabs() {
                                 </span>
                             </button>
                         </div>
-                        <div className={styles['c-projectsTabs__descriptions']} style={{height: `${tabsWrapperHeight}px`}}>
-                            <div
-                                data-id={0}
-                                key={0}
-                                className={classNames(
-                                    'o-wysiwyg',
-                                    styles['c-projectsTabs__descriptions--element'],
-                                    {
-                                        [styles['is-selected']]: activeTabIndex == 0
-                                    }
-                                )}
-                                ref={(el) => tabsItemsRef.current[0] = el}
+                        <div className={styles['c-projectsTabs__descriptions']} ref={tabsWrapperRef}>
+                            <FadeInOut
+                                delay={0.53}
                             >
-                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate inventore nisi aut ad est quo sunt cupiditate cum nemo quam.</p>
-                            </div>
-                            <div
-                                data-id={1}
-                                key={1}
-                                className={classNames(
-                                    'o-wysiwyg',
-                                    styles['c-projectsTabs__descriptions--element'],
-                                    {
-                                        [styles['is-selected']]: activeTabIndex == 1
-                                    }
-                                )}
-                                ref={(el) => tabsItemsRef.current[1] = el}
-                            >
-                                {/* <p>Cras pulvinar mattis nunc sed blandit libero. Molestie nunc non blandit massa. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Vitae ultrici.</p> */}
-                                <p>Consectetur adipisicing elit. Porro aliquam eius accusamus maxime necessitatibus, itaque reiciendis architecto voluptates at quisquam adipisci nostrum tempore, minima deserunt, sequi incidunt repellendus officiis veniam.</p>
-                            </div>
+                                <div
+                                    data-id={0}
+                                    key={0}
+                                    className={classNames(
+                                        'o-wysiwyg',
+                                        styles['c-projectsTabs__descriptions--element'],
+                                        {
+                                            [styles['is-selected']]: activeTabIndex == 0
+                                        }
+                                    )}
+                                    ref={(el: HTMLDivElement) => tabsItemsRef.current[0] = el}
+                                >
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae voluptate inventore nisi aut ad est quo sunt cupiditate cum nemo quam.</p>
+                                </div>
+                                <div
+                                    data-id={1}
+                                    key={1}
+                                    className={classNames(
+                                        'o-wysiwyg',
+                                        styles['c-projectsTabs__descriptions--element'],
+                                        {
+                                            [styles['is-selected']]: activeTabIndex == 1
+                                        }
+                                    )}
+                                    ref={(el: HTMLDivElement) => tabsItemsRef.current[1] = el}
+                                >
+                                    {/* <p>Cras pulvinar mattis nunc sed blandit libero. Molestie nunc non blandit massa. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Vitae ultrici.</p> */}
+                                    <p>Consectetur adipisicing elit. Porro aliquam eius accusamus maxime necessitatibus, itaque reiciendis architecto voluptates at quisquam adipisci nostrum tempore, minima deserunt, sequi incidunt repellendus officiis veniam.</p>
+                                </div>
+                            </FadeInOut>
                         </div>
                     </div>
                 </div>
