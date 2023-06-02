a1406f9cbb9380c2bd3acd859e30f72b35aa0a97
gcolombi <gg_1605@hotmail.fr>
2023-06-01 21:08:26 -0400

    update ProjectsTabs - add animations

--- components/ProjectsTabs.tsx
+++ components/ProjectsTabs.tsx
@@ -1,8 +1,8 @@
 import styles from '@/styles/modules/ProjectsTabs.module.scss';
-import classNames from 'classnames';
 import FadeInOut from './shared/gsap/FadeInOut';
-
-console.log(styles);
+import CharsInOut from './shared/gsap/CharsInOut';
+import TranslateInOut from './shared/gsap/TranslateInOut';
+import classNames from 'classnames';
 
 export default function ProjectsTabs() {
     return (
@@ -13,7 +13,9 @@ export default function ProjectsTabs() {
                         'h4',
                         styles['c-projectsTabs__index']
                     )}>
-                        <FadeInOut>
+                        <FadeInOut
+                            delay={0.53}
+                        >
                             <span>
                                 01
                             </span>
@@ -26,12 +28,52 @@ export default function ProjectsTabs() {
                                 [styles['is-active']]: true
                             }
                         )}>
-                            <span className="h1">Projects</span>
-                            <span className={styles['c-projectsTabs__header__control--count']}>04</span>
+                            <div className="h1">
+                                <CharsInOut
+                                    delay={0.46}
+                                    target="#project-type-1"
+                                >
+                                    <span id="project-type-1">
+                                        Work
+                                    </span>
+                                </CharsInOut>
+                            </div>
+                            <span className={classNames(
+                                'u-overflow--hidden',
+                                styles['c-projectsTabs__header__control--count']
+                            )}>
+                                <TranslateInOut
+                                    fade={false}
+                                    delay={0.53}
+                                    y="100%"
+                                >
+                                    04
+                                </TranslateInOut>
+                            </span>
                         </button>
                         <button className={styles['c-projectsTabs__header__control']}>
-                            <span className="h1">Personal</span>
-                            <span className={styles['c-projectsTabs__header__control--count']}>03</span>
+                            <div className="h1">
+                                <CharsInOut
+                                    delay={0.46}
+                                    target="#project-type-1"
+                                >
+                                    <span id="project-type-1">
+                                        Personal
+                                    </span>
+                                </CharsInOut>
+                            </div>
+                            <span className={classNames(
+                                'u-overflow--hidden',
+                                styles['c-projectsTabs__header__control--count']
+                            )}>
+                                <TranslateInOut
+                                    fade={false}
+                                    delay={0.53}
+                                    y="100%"
+                                >
+                                    03
+                                </TranslateInOut>
+                            </span>
                         </button>
                     </div>
                 </div>
