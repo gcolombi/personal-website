5c55ea754f1b9b92ff30576388cfe1c2a802493c
gcolombi <gg_1605@hotmail.fr>
2023-06-13 23:17:25 -0400

    add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
                 <link rel="manifest" href="/static/favicons/site.webmanifest" />
                 <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#000000" />
                 <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
-                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-TileColor" content="#e1dfdd" />
                 <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
                 <meta name="theme-color" content="#ffffff" />
             </Head>
