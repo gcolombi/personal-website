d4cebda451f8c19dc028de999b801ac4199ccba5
gcolombi <gg_1605@hotmail.fr>
2023-06-21 13:52:03 -0400

    update MetaData - remove debug

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -1,6 +1,7 @@
 import { MetaDataProps } from '@/types/components/global';
 import Head from 'next/head';
 import useWindowLocation from '@/hooks/useWindowLocation';
+import { useEffect } from 'react';
 
 export default function MetaData({
     title = 'Gerard Colombi',
@@ -10,19 +11,26 @@ export default function MetaData({
 }: MetaDataProps) {
     const { currentURL } = useWindowLocation();
 
+    useEffect(() => {
+        console.log(title);
+        console.log(description);
+        console.log(image);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <Head>
             <meta charSet="utf-8" />
         	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Gerard Colombi</title>
-            <meta name="description" content="Front-end developer capable of full stack development, passionate about building appealing and interactive web experiences." />
-            <meta property="og:title" content="Gerard Colombi" />
-            <meta property="og:description" content="Front-end developer capable of full stack development, passionate about building appealing and interactive web experiences." />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
             <meta property="og:url" content={currentURL} />
             <meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
             <meta property="og:type" content={type} />
-            <meta property="og:image" content={`${currentURL}static/og-image.png`} />
+            <meta property="og:image" content={image} />
             <meta name="robots" content="follow, index" />
             <link rel="canonical" href={currentURL} />
         </Head>
