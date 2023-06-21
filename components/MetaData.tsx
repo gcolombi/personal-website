b0a61898b8faf93505caf9d6d63a3038cfec755e
gcolombi <gg_1605@hotmail.fr>
2023-06-21 13:11:08 -0400

    update MetaData - debug

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -15,14 +15,14 @@ export default function MetaData({
             <meta charSet="utf-8" />
         	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
+            <title>Gerard Colombi</title>
+            <meta name="description" content="Front-end developer capable of full stack development, passionate about building appealing and interactive web experiences." />
+            <meta property="og:title" content="Gerard Colombi" />
+            <meta property="og:description" content="Front-end developer capable of full stack development, passionate about building appealing and interactive web experiences." />
             <meta property="og:url" content={currentURL} />
             <meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
             <meta property="og:type" content={type} />
-            <meta property="og:image" content={image} />
+            <meta property="og:image" content={`${currentURL}static/og-image.png`} />
             <meta name="robots" content="follow, index" />
             <link rel="canonical" href={currentURL} />
         </Head>
