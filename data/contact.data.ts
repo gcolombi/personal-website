11c37c02f4d6af7b402678808ca6fa5a9c302272
gcolombi <gg_1605@hotmail.fr>
2023-06-23 19:12:58 -0400

    update contact page - add contact header data

--- data/contact.data.ts
+++ data/contact.data.ts
@@ -10,7 +10,18 @@ export const META_CONTACT: Lang<MetaDataProps> = {
     }
 };
 
-export const CONTACT_HEADER: BasicHeaderProps = {
-    title: 'Contact',
-    content: 'Is there something on your mind you\'d like to talk about? Whether it\'s related to work, just a casual conversation or need help with some code. Feel free to contact me at anytime.'
+export const CONTACT_HEADER: Lang<BasicHeaderProps> = {
+    en: {
+        title: 'Contact',
+        content: 'Is there something on your mind you\'d like to talk about? Whether it\'s related to work, just a casual conversation or need help with some code. Feel free to contact me at anytime.'
+    },
+    fr: {
+        title: 'Contact',
+        content: 'Il y a quelque chose qui vous préoccupe et dont vous aimeriez parler ? Que ce soit dans le cadre du travail, d\'une conversation informelle ou besoin d\'aide avec du code. N\'hésitez pas à me contacter à tout moment.'
+    }
 };
+
+// export const CONTACT_HEADER: BasicHeaderProps = {
+//     title: 'Contact',
+//     content: 'Is there something on your mind you\'d like to talk about? Whether it\'s related to work, just a casual conversation or need help with some code. Feel free to contact me at anytime.'
+// };
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,17 @@
 import { CONTACT_HEADER, META_CONTACT } from '@/data/contact.data';
 import { MetaDataProps } from '@/types/components/global';
-import { GetStaticProps } from 'next';
+import { BasicHeaderProps } from '@/types/components/headers';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import BasicHeader from '@/components/BasicHeader';
 import Form from '@/components/form/Form';
 
-export default function Contact() {
+export default function Contact({
+    contactHeader
+}: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <>
             <BasicHeader
-                {...CONTACT_HEADER}
+                {...contactHeader}
             />
             <Form />
         </>
@@ -17,14 +20,19 @@ export default function Contact() {
 
 export const getStaticProps: GetStaticProps<{
     metaData: MetaDataProps;
+    contactHeader: BasicHeaderProps;
 }> = async ({ locale }) => {
     const lang = locale ?? '';
     const metaContact = META_CONTACT[lang];
+    const contactHeader = CONTACT_HEADER[lang];
 
     return {
         props: {
             metaData: {
                 ...metaContact
+            },
+            contactHeader: {
+                ...contactHeader
             }
         }
     }
