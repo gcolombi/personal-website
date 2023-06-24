7d034e8e53479f9ce4284abe4171d166dcc11d57
gcolombi <gg_1605@hotmail.fr>
2023-06-23 20:05:56 -0400

    update 404 - move META in staticProps

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,11 @@
 import { META_404, PAGE_NOT_FOUND_HEADER } from '@/data/global.data';
+import { MetaDataProps } from '@/types/components/global';
+import { GetStaticProps } from 'next';
 import BasicHeader from '@/components/BasicHeader';
-import MetaData from '@/components/MetaData';
 
 export default function PageNotFound() {
     return (
         <>
-            <MetaData
-                {...META_404}
-            />
             <BasicHeader
                 {...PAGE_NOT_FOUND_HEADER}
                 button={PAGE_NOT_FOUND_HEADER?.button ? {...PAGE_NOT_FOUND_HEADER.button, href: '/'} : undefined}
@@ -15,4 +13,14 @@ export default function PageNotFound() {
             />
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps<{metaData: MetaDataProps}> = async () => {
+    return {
+        props: {
+            metaData: {
+                ...META_404
+            }
+        }
+    }
 }
