2d8f6f78d9dff2b96a299e00631d7da455216d40
gcolombi <gg_1605@hotmail.fr>
2023-06-24 19:15:22 -0400

    update LanguageSwitcher

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,22 +1,28 @@
 import { LanguageSwitcherProps } from '@/types/components/global';
 import { Link, translateUrl } from 'next-translate-routes';
 import { capitalizeFirstLetter } from '@/utils/string';
+import { useEffect, useState } from 'react';
 
 export default function LanguageSwitcher({
     router
 }: LanguageSwitcherProps) {
+    const [locales, setLocales] = useState<string[]>([]);
+
     const getLocales = () => {
         const locales = router.locales ?? [];
         return locales.filter(l => l !== router.locale);
     }
-
-    const locales = getLocales();
 
     const switchToLocale = () => {
         router.events.on('routeChangeComplete', () => {
             router.reload();
         });
     }
+
+    useEffect(() => {
+        setLocales(getLocales());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const href = router.route !== '/404' ? translateUrl(router.pathname, router.locale ?? '') : '/';
 
