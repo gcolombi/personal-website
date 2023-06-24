ed9ccb038c85c2ec513518827268183ef3e34488
gcolombi <gg_1605@hotmail.fr>
2023-06-24 13:49:50 -0400

    update about - data WIP

--- data/about.data.ts
+++ data/about.data.ts
@@ -11,15 +11,30 @@ export const META_ABOUT: Lang<MetaDataProps> = {
     }
 };
 
-export const ABOUT_HEADER: AboutHeaderProps = {
-    titles: ['Passionate', 'Front-end', 'Developer'],
-    image: '/static/images/about-header.jpg'
+export const ABOUT_HEADER: Lang<AboutHeaderProps> = {
+    en: {
+        titles: ['Passionate', 'Front-end', 'Developer'],
+        image: '/static/images/about-header.jpg'
+    },
+    fr: {
+        titles: ['Developpeur', 'Front-end', 'Passionne'],
+        image: '/static/images/about-header.jpg'
+    }
 };
 
-export const ABOUT_INTRODUCTION: AboutIntroductionContent = {
-    content: [
-        'I\'m a front-end developer with years of experiences in building performant, accessible and responsive applications/websites. I like to code things from scratch, and enjoy bringing ideas to life in the browser. The life of a developer requires one to learn new things everyday, because technology keeps changing at a very fast pace. I have therefore, embraced the concept of being a life long learner and continue to learn new concepts and technologies in my free time.',
-        'The things we make and the quality of the work we do reveal something about who we are, so I always strive to put my best into it.'
-    ],
-    image: '/static/images/about-portrait.jpg'
+export const ABOUT_INTRODUCTION: Lang<AboutIntroductionContent> = {
+    en: {
+        content: [
+            'I\'m a front-end developer with years of experiences in building performant, accessible and responsive applications/websites. I like to code things from scratch, and enjoy bringing ideas to life in the browser. The life of a developer requires one to learn new things everyday, because technology keeps changing at a very fast pace. I have therefore, embraced the concept of being a life long learner and continue to learn new concepts and technologies in my free time.',
+            'The things we make and the quality of the work we do reveal something about who we are, so I always strive to put my best into it.'
+        ],
+        image: '/static/images/about-portrait.jpg'
+    },
+    fr: {
+        content: [
+            'Je suis un développeur front-end avec des années d\'expérience dans la création d\'applications/sites web performants, accessibles et réactifs. J\'aime coder les choses à partir de zéro et donner vie à mes idées dans le navigateur. La vie d\'un développeur exige que l\'on apprenne de nouvelles choses tous les jours, car la technologie évolue à un rythme très rapide. J\'ai donc adopté le concept d\'apprentissage tout au long de ma vie et je continue à apprendre de nouveaux concepts et nouvelles technologies pendant mon temps libre.',
+            'Les choses que nous créons et la qualité du travail que nous effectuons révèlent quelque chose sur notre personnalité, c\'est pourquoi je m\'efforce toujours de donner le meilleur de moi-même.'
+        ],
+        image: '/static/images/about-portrait.jpg'
+    }
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,8 @@
 import { ABOUT_HEADER, ABOUT_INTRODUCTION, META_ABOUT } from '@/data/about.data';
 import { HOBBIES_TABS, HOBBIES_TITLE, MODELS } from '@/data/hobbies.data';
 import { CALL_TO_ACTION } from '@/data/global.data';
+import { AboutHeaderProps } from '@/types/components/headers';
+import { AboutIntroductionContent } from '@/types/components/introductions';
 import { CallToActionContent, MetaDataProps } from '@/types/components/global';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import AboutHeader from '@/components/AboutHeader';
@@ -9,16 +11,18 @@ import HobbiesTabs from '@/components/HobbiesTabs';
 import CallToAction from '@/components/CallToAction';
 
 export default function About({
+    aboutHeader,
+    aboutIntroduction,
     callToAction
 }: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <>
             <AboutHeader
-                {...ABOUT_HEADER}
+                {...aboutHeader}
             />
             <AboutIntroduction
                 index="01"
-                {...ABOUT_INTRODUCTION}
+                {...aboutIntroduction}
             />
             <HobbiesTabs
                 index="02"
@@ -36,15 +40,21 @@ export default function About({
 
 export const getStaticProps: GetStaticProps<{
     metaData: MetaDataProps;
+    aboutHeader: AboutHeaderProps;
+    aboutIntroduction: AboutIntroductionContent;
     callToAction: CallToActionContent;
 }> = async ({ locale }) => {
     const lang = locale ?? '';
     const metaData = META_ABOUT[lang];
+    const aboutHeader = ABOUT_HEADER[lang];
+    const aboutIntroduction = ABOUT_INTRODUCTION[lang];
     const callToAction = CALL_TO_ACTION[lang];
 
     return {
         props: {
             metaData,
+            aboutHeader,
+            aboutIntroduction,
             callToAction
         }
     }
