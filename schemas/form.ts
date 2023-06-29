d6fd305e904febcbffa168a1347762db3a06668f
gcolombi <gg_1605@hotmail.fr>
2023-06-28 20:47:44 -0400

    update form schema - add missing doc

--- schemas/form.ts
+++ schemas/form.ts
@@ -2,7 +2,12 @@ import { FormData } from '@/types/form';
 import { object, string, addMethod, ObjectSchema } from 'yup';
 import { getTranslation } from '@/utils/translation';
 
-export const getFormSchema = (locale: string) => {
+/**
+ * Returns form schema for validation
+ * @param {string} locale the locale used for translations
+ * @returns {ObjectSchema<FormData>} the form schema
+ */
+export const getFormSchema = (locale: string): ObjectSchema<FormData> => {
     /* Override the email method, if email isn't required we need to add excludeEmptyString: true */
     addMethod(string, 'email', function validateEmail(message: string){
         return this.matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, {
