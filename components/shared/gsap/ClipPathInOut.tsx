939aaf6c21f37e826ab99d19b61e734e21d1c96e
gcolombi <gg_1605@hotmail.fr>
2023-07-11 21:06:44 -0400

    update ClipPathInOut - handle locale change

--- components/shared/gsap/ClipPathInOut.tsx
+++ components/shared/gsap/ClipPathInOut.tsx
@@ -1,8 +1,11 @@
 import { ClipPath } from '@/types/animations';
-import { gsap } from 'gsap';
-import { useRef } from 'react';
-import useTransitionContext from '@/context/transitionContext';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import { useRef, useState } from 'react';
 import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+import useNavigationContext from '@/context/navigationContext';
+import useTransitionContext from '@/context/transitionContext';
+import { useRouter } from 'next-translate-routes';
 
 export default function ClipPathInOut({
     children,
@@ -23,8 +26,11 @@ export default function ClipPathInOut({
     scrub = false,
     markers
 }: ClipPath) {
+    const { locale } = useRouter();
+    const { currentLocale } = useNavigationContext();
     const { timeline, primaryEase } = useTransitionContext();
     const element = useRef<HTMLDivElement | null>(null);
+    const [animation, setAnimation] = useState<GSAPTween | null>(null);
 
     useIsomorphicLayoutEffect(() => {
         const scrollTrigger = watch ? {
@@ -40,19 +46,20 @@ export default function ClipPathInOut({
         const ctx = gsap.context(() => {
             /* Intro animation */
             gsap.fromTo(element.current,
-            {
-                opacity: fade ? 0 : 1,
-                clipPath,
-                ease: ease ?? primaryEase
-            },
-            {
-                opacity: 1,
-                clipPath: clipPathTo,
-                ease: ease ?? primaryEase,
-                delay,
-                duration: durationIn,
-                ...scrollTrigger
-            });
+                {
+                    opacity: fade ? 0 : 1,
+                    clipPath,
+                    ease: ease ?? primaryEase
+                },
+                {
+                    opacity: 1,
+                    clipPath: clipPathTo,
+                    ease: ease ?? primaryEase,
+                    delay,
+                    duration: durationIn,
+                    ...scrollTrigger
+                }
+            );
 
             /* Outro animation */
             if (!skipOutro) {
@@ -77,6 +84,66 @@ export default function ClipPathInOut({
         }, element);
         return () => ctx.revert();
     }, []);
+
+    useIsomorphicLayoutEffect(() => {
+        if (currentLocale !== locale) {
+            /* Kill old animation */
+            animation?.kill();
+
+            const isInViewport = ScrollTrigger.isInViewport(element.current as Element);
+            const isAboveViewport = ScrollTrigger.positionInViewport(element.current as Element, 'bottom') <= 0;
+            let anim: GSAPTween | null = null;
+
+            /* Intro animation */
+            if (!isInViewport && !isAboveViewport) {
+                const scrollTrigger = watch ? {
+                    scrollTrigger: {
+                        trigger: element.current,
+                        start,
+                        end,
+                        scrub,
+                        markers: markers
+                    }
+                } : {};
+                
+                anim = gsap.fromTo(element.current,
+                    {
+                        opacity: fade ? 0 : 1,
+                        clipPath,
+                        ease: ease ?? primaryEase
+                    },
+                    {
+                        opacity: 1,
+                        clipPath: clipPathTo,
+                        ease: ease ?? primaryEase,
+                        delay,
+                        duration: durationIn,
+                        ...scrollTrigger
+                    }
+                );
+
+                setAnimation(anim);
+            } else {
+                setAnimation(anim);
+            }
+
+            /* Outro animation */
+            if (!skipOutro) {
+                timeline?.add(
+                    gsap.to(
+                        element.current,
+                        {
+                            clipPath: clipPathOut ?? clipPath,
+                            ease: easeOut ?? primaryEase,
+                            delay: delayOut,
+                            duration: durationOut
+                        }
+                    ),
+                    0
+                );
+            }
+        }
+    }, [locale]);
 
     return (
         <div ref={element} style={{ opacity: 0 }}>
